e883a8505f8c699507977dfac267ba2309534714
陈聪 <donglizheng@chencongdemac-mini.local>
2016-08-04 15:44:03 +0800

    Initial Commit

---
+++ GPC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPC
+//
+//  Created by 董立峥 on 16/8/4.
+//  Copyright © 2016年 董立峥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GPC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPC
+//
+//  Created by 董立峥 on 16/8/4.
+//  Copyright © 2016年 董立峥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
